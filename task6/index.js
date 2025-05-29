f479a6c3c8ae396bc59e00806d0fce416c6af894
zheka171717 <zhena.kondrashov@mail.ru>
2025-05-29 16:58:24 +0300

    Create index.js

---
+++ task6/index.js
@@ -0,0 +1,9 @@
+
+const content = document.querySelector('.content');
+const button = document.createElement('button');
+button.textContent='Отправить';
+content.appendChild(button);
+
+button.addEventListener('click', (event)=> {
+    event.target.textContent='Текст отправлен';
+})
